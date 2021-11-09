e0132152a47457ad1eab6554d6a9e17facda407e
yaxin <leepace666666@sjtu.edu.cn>
2021-11-09 13:40:37 +0800

    adapt parser, ast, scanner for helloworld

--- src/ast.ml
+++ src/ast.ml
@@ -1,9 +1,9 @@
-type op = Add | Sub | Mul | Div | Equal | Neq | Less | Leq | Greater | Geq |
+type op = Add | Sub | Mul | Div | Mod | Equal | Neq | Less | Leq | Greater | Geq |
           And | Or
 
 type uop = Neg | Not
 
-type typ = Bool | Int | Float | Char | StructID | Node | Edge | Graph
+type typ = Bool | Int | Float | Char | String | StructID | Node | Edge | Graph
 
 type bind = typ * string
 
@@ -14,21 +14,29 @@ type expr =
 |   Id of string
 |   IntLit of int
     (* Function Call *)
+|   Call of string * expr list
+|   Noexpr
 
 type stmt = 
     Expr of expr
-|   If of expr * stmt * expr
+|   Vdecl of bind
+|   Block of stmt list
+|   If of expr * stmt * stmt
 |   For of expr * expr * expr * stmt
 |   While of expr * stmt
 |   Break
 |   Continue
 |   Return of expr
 
-(*type fun_decl = {}*)
+type func_decl = {
+    typ : typ;
+    fname : string;
+    formals : bind list;
+    (*locals : bind list;*) (*TODO*)
+    body : stmt list;
+  }
 
-(*type program = bind list * fun_decl list*)
-
-
+type program = bind list * func_decl list
 
 let string_of_op = function
     Add -> "+"
@@ -59,6 +67,7 @@ let string_of_typ = function
 |   Graph -> "graph"
     (* typ LBRACKET RBRACKET *)
 
+let string_of_bind (t, id) = string_of_typ t ^ " " ^ id ^ ";\n" 
 
 let rec string_of_expr = function
     Binop(e1, o, e2) -> 
@@ -72,6 +81,7 @@ let rec string_of_expr = function
 let rec string_of_stmt = function
     Expr(e) -> string_of_expr e ^ ";\n"
 (*|   If(e, s) -> "if (" ^ string_of_expr e ^ ")\n" ^ string_of_stmt s*)
+|   Vdecl(b) -> string_of_bind b
 |   For(e1, e2, e3, s) ->
       "for (" ^ string_of_expr e1  ^ " ; " ^ string_of_expr e2 ^ " ; " ^
       string_of_expr e3  ^ ") " ^ string_of_stmt s
@@ -83,5 +93,15 @@ let rec string_of_stmt = function
 let string_of_vdecl (t, id) = string_of_typ t ^ " " ^ id ^ ";\n"
 
 (* string_of_fdecl *)
+let string_of_fdecl fdecl =
+  string_of_typ fdecl.typ ^ " " ^
+  fdecl.fname ^ "(" ^ String.concat ", " (List.map snd fdecl.formals) ^
+  ")\n{\n" ^
+  (*String.concat "" (List.map string_of_vdecl fdecl.locals) ^*)(*TODO*)
+  String.concat "" (List.map string_of_stmt fdecl.body) ^
+  "}\n"
 
 (* string_of_program *)
+let string_of_program (vars, funcs) =
+  (*String.concat "" (List.map string_of_vdecl vars) ^ "\n" ^*) (*TODO*)
+  String.concat "\n" (List.map string_of_fdecl funcs)
